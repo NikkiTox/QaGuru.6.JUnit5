f61221be17bbc510340e0f67c73b64bd0744d962
Nikkitox <Niki.tone@yandex.ru>
2022-03-13 21:28:15 +0500

    added edits for homework

--- src/test/java/guru/qa/ParametrizedSearchTest.java
+++ src/test/java/guru/qa/ParametrizedSearchTest.java
@@ -1,6 +1,6 @@
 package guru.qa;
 
-import com.codeborne.selenide.Condition;
+
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.*;
@@ -13,12 +13,18 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 
 public class ParametrizedSearchTest {
+    @BeforeAll
+   public static void preconditionBrowser(){
+        Configuration.browserSize = "1920x1080";
+    }
+
     @BeforeEach
     void precondition() {
-        Configuration.browserSize = "1920x1080";
+
         Selenide.open("https://demoqa.com/text-box");
     }
 
@@ -29,14 +35,14 @@ public class ParametrizedSearchTest {
 
 
     @ValueSource(strings = {"Ivanov", "Petrov"})
-    @ParameterizedTest(name = "Проверка отображение правильности регистрации DemoQA \"{0}\"")
+    @ParameterizedTest(name = "Проверка отображение правильности регистрации DemoQA с помощью Value Source \"{0}\"")
     void commonSearchTest(String testData) {
         $("#userName").setValue(testData);
         $("#userEmail").setValue(testData + "@mail.ru");
         $("#currentAddress").setValue("Moscow");
         $("#permanentAddress").setValue("Kremlin");
         $("#submit").click();
-        $("#output").shouldHave(Condition.text(testData));
+        $("#output").shouldHave(text(testData));
     }
 
     //@Disabled
@@ -46,25 +52,34 @@ public class ParametrizedSearchTest {
     },
             delimiter = '*'
     )
-    @ParameterizedTest(name = "Проверка отображение правильности регистрации DemoQA \"{0}\"")
+    @ParameterizedTest(name = "Проверка отображение правильности регистрации DemoQA с Parameterized Test \"{0}\"")
     void complexSearchTest(String testData, String expectedText) {
         $("#userName").setValue(testData);
         $("#userEmail").setValue(testData + "@mail.ru");
         $("#currentAddress").setValue(expectedText);
         $("#permanentAddress").setValue(expectedText);
+        $("#submit").click();
+        $("#output").shouldHave(text(testData));
     }
 
     static Stream<Arguments>mixedArgumentsTestDataProvider(){
         return Stream.of(
-                Arguments.of("Ivanov", List.of("Moscow","Kremlin"), true),
-                Arguments.of("Petrov", List.of("St.Petersburg","Kronshtadt"),true)
-        );
+                Arguments.of("Ivanov", List.of("Moscow","Kremlin","Red Square"),
+                Arguments.of("Petrov", List.of("St.Petersburg","Kronshtadt","Kotlin Island")
+                )));
 
     }
     //@Disabled
     @MethodSource(value ="mixedArgumentsTestDataProvider")
     @ParameterizedTest
-    void mixedArgumentsTest(String firstArg, List<String> secondArg, boolean aBooleanValue){
-        System.out.println("String:" + firstArg + "list:" + secondArg.toString()+ "boolean" + aBooleanValue);
+    void mixedArgumentsTest(String firstArg, List<String> secondArg){
+        $("#userName").setValue(firstArg);
+        $("#userEmail").setValue(firstArg + "@mail.ru");
+        $("#currentAddress").setValue(String.valueOf(secondArg));
+        $("#permanentAddress").setValue(String.valueOf(secondArg));
+        $("#submit").click();
+        $("#output").shouldHave(text(firstArg));
+
+
     }
 }
